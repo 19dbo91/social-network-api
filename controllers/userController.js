6006e17e774d37c6c746b419d60c41115057177a
Dustin Bonilla <bonilla.dustin@gmail.com>
2023-08-30 01:25:53 -0700

    updated delete user, updated add friend to user, confirmed working

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
 const User = require('../models/User');
 
 module.exports = {
-    async getAllUsers(req,res){
+    async getAllUsers(req,res){ // // TODO: verify route - GOOD
         try {
             const users = await User.find();
             res.status(200).json(users);
@@ -9,7 +9,7 @@ module.exports = {
             res.status(500).json(err);
         }
     },
-    async getUser(req,res){
+    async getUser(req,res){ // // TODO: verify route - GOOD
         try {
             const user = await User.findOne({ _id: req.params.userId });
 
@@ -21,7 +21,7 @@ module.exports = {
             res.status(500).json(err);
         }
     },
-    async createUser(req,res){
+    async createUser(req,res){ // // TODO: verify route - GOOD
         try {
             const newUser = await User.create({
                 username: req.body.username,
@@ -32,7 +32,7 @@ module.exports = {
             res.status(500).json(err);
         }
     },
-    async updateUser(req,res){
+    async updateUser(req,res){ // // TODO: verify route - GOOD
         try {
             const userId = req.params.userId;
             const {username, email}= req.body;
@@ -40,7 +40,7 @@ module.exports = {
                 { _id: userId },
                 { username, email }
             );
-            console.log(user)
+            // console.log(user)
             if (!user) {
                 return res.status(404).json({ message: 'No user with that ID' });
             }
@@ -49,52 +49,53 @@ module.exports = {
             res.status(500).json(err);
         }
     },
-    async deleteUser(req,res){
+    async deleteUser(req,res){ // // TODO: verify route - GOOD
         try {
-            const deletingUser = await User.findOneAndDelete({ _id: req.body.userId });
+            const deletingUser = await User.findOneAndDelete({ _id: req.params.userId });
+            
             if(!deletingUser){
-                res.status(404).json(deletingUser); 
+                res.status(404).json({ message: 'No user with that ID' }); 
             }
             res.status(200).json(deletingUser);
         } catch (err) {
             res.status(500).json(err);
         }
     },
-    async addToFriends(req,res){
+    async addToFriends(req,res){ // // TODO: verify route - GOOD
         try {
             
             const existingUser = await User.findOne({ _id: req.params.userId });
+            
+            
             // Checking user exists
             if (!existingUser) {
                 return res.status(404).json({ message: 'No user with that ID' });
             }
-
+            
             // Checking users friends first to reduce #api calls
             let usersfriends = existingUser.friends;
             const friendIndex = usersfriends.findIndex( (friend) => {
                 friend === req.params.friendId
             });
-            
             if (friendIndex >= 0){
-                
                 return res.status(200).json({ message: 'User already is friend with that user' });
             }
             
 
             // Checking if friend exists 
-            const friend = await User.findOne({ _id: req.params.friendId });
-            
-            if (!friend){
+            const newFriend = await User.findOne({ _id: req.params.friendId });
+            if (!newFriend){
                 return res.status(404).json({ message: 'No friend with that ID' });
             }
             
-            const newFriendsList = usersfriends.push(req.params.friendId);
-            console.log(usersfriends)
-            const updatedFriends = await User.update(
+            // Update User list
+            const newFriendsList = [...usersfriends, req.params.friendId];
+            console.log(newFriendsList)
+            const updatedFriends = await User.updateOne(
                 {_id: req.params.userId},
-                {friends:newFriendsList}
+                {friends: newFriendsList}
             )
-            
+            console.log(updatedFriends)
 
             res.status(200).json(updatedFriends);
         } catch (err) {
